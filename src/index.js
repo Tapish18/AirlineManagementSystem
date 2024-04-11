d5501d5c2aad7431782b1cdc1961ded158c2851a
Tapish Verma <tapishverma176@gmail.com>
2024-04-11 16:11:04 +0530

    et up mysql db with sequelize ORM.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log("Error Occurred : " + err);
-  } else {
-    console.log("Server Started sucessfully at port : " + PORT);
-  }
-});
+const startAndSetupServer = async () => {
+  // Adding middlewares
+  app.use(bodyParser.json);
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, (err) => {
+    if (err) {
+      console.log("Error Occurred : " + err);
+    } else {
+      console.log("Server Started sucessfully at port : " + PORT);
+    }
+  });
+};
+
+startAndSetupServer();
